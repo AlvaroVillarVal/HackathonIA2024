6dfecd24a992eb1d4d790b88dfc4f4dc0a54314c
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-03-08 17:33:01 +0100

    Prototypo muestra datos

--- Prototipo.py
+++ Prototipo.py
@@ -1,20 +1,25 @@
-class AI:
-    def __init__(self):
-        self.difficulty = 1
+import pandas as pd
 
-    def adapt_difficulty(self, score):
-        if score > 10:
-            self.difficulty += 1
-        elif score < 5:
-            self.difficulty -= 1
+# Path to the CSV file
+import matplotlib.pyplot as plt
 
-    def play(self):
-        # AI logic based on current difficulty
-        pass
+csv_file = 'RetoIA\datasetIA.csv'
 
-# Create an instance of the AI
-ai = AI()
+# Read the CSV file using pandas
+data = pd.read_csv(csv_file,sep=';')
+for col in data.columns:
+    data[col] = pd.to_numeric(data[col], errors='coerce')
+# Define a function to show a graph of a column
+def show_graph(column_name):
+    # Plot the column as a graph
+    data[column_name].plot()
+    # Display the graph
+    plt.show()
+columnas = ["Trial values", "Aciertos", "Fallos", "Recompensa minima", "Recompensa maxima", "Puntuación maxima", "Tiempo de respuesta min", "Tiempo de respuesta maximo", "Tiempo total de la prueba", "Distancia al jugador de los objetos", "Numero de piezas", "Ratio de aparición de los objetos en el juego", "Ratio de recompensa de los objetos", "Tamaño de los objetos"]
 
-# Example usage
-ai.adapt_difficulty(8)  # Adjust difficulty based on score
-ai.play()  # Perform AI actions based on current difficulty
+# Call the function to show a graph of a specific column
+show_graph(columnas[1])
+print(data['Aciertos'].dtypes)
+# Display the data
+print(data.columns)
+print(data['Trial values'])
